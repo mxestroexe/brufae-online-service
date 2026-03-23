62da6f2dd9e7568c7cebe4f26fea1296ac3991a3
João Lucca <cort3zzzzin@gmail.com>
2026-03-22 22:01:43 -0300

    python atualizado

---
+++ pages/code/index.py
@@ -0,0 +1,9 @@
+desperdicio_energia = 0
+
+if desperdicio_energia() = True:
+    print("está tendo desperdicio_energia")
+else:
+    if desperdicio_energia() = False:
+        print("não há desperdício")
+elif desperdicio_energia() = null:
+    print(não sei oq deu)
