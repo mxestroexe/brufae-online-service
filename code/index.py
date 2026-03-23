255020ee17a26d1118998256eb4f8ef9f0d30393
João Lucca <cort3zzzzin@gmail.com>
2026-03-23 14:43:08 -0300

    pasta code foi separada de pages

---
+++ code/index.py
@@ -0,0 +1,13 @@
+def desperdicio_energia():
+    # Exemplo de função que determina se há desperdício de energia
+    # Retorna True, False ou None
+    return None
+
+resultado = desperdicio_energia()
+
+if resultado is True:
+    print("está tendo desperdicio_energia")
+elif resultado is False:
+    print("não há desperdício")
+else:
+    print("não sei oq deu")
